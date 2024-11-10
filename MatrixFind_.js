62122ea96ffed23ec4aac2fc621d8f98b706db27
Nirmal Kumar <kumar.nirmal2608@gmail.com>
2024-11-10 07:20:41 +0530

    CodeBase Updated

--- MatrixFind_.js
+++ MatrixFind_.js
@@ -9,21 +9,5 @@ function getMatrix() {
       alert("Please enter a valid matrix size.");
       return;
     }
-  
-    const matrixInputDiv = document.getElementById("matrixInput");
-    matrixInputDiv.innerHTML = "";
-  
-    for (let i = 0; i < size; i++) {
-      const rowDiv = document.createElement("div");
-      rowDiv.className = "matrix-row";
-      for (let j = 0; j < size; j++) {
-        const input = document.createElement("input");
-        input.type = "number";
-        input.id = `matrix-${i}-${j}`;
-        rowDiv.appendChild(input);
-      }
-      matrixInputDiv.appendChild(rowDiv);
-    }
-  
-    document.getElementById("calculateButton").style.display = "inline-block";
+    document.write('Enter JavaScript Codes')
   }
